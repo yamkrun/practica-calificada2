5e41804186cb0d16465f8d33be975d1bddc222f7
Yamila Rengifo <yamirngifo18@gmail.com>
2025-08-14 23:17:54 -0300

    botones y fuente

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,26 @@ function App() {
   return (
     <>
       <header className="flex justify-center items-center">
-        <h1 className="text-pink-500 font-bold text-2xl my-8">Photo Gallery</h1>
+        <h1 className="tit text-pink-500 text-3xl my-10">Photo Gallery</h1>
       </header>
 
       <main className="flex flex-col justify-center items-center">
         <Search valorIngreso={search1} setValorIngreso={setSearch1} />
-        <section className="grid grid-cols-2 sm:grid-cols-4 mt-6 gap-4">
-          <Buttons category={"Mountain"} />
-          <Buttons category={"Flowers"} />
-          <Buttons category={"Sea"} />
-          <Buttons category={"Faces"} />
+        <section className="grid grid-cols-3 sm:grid-cols-6 mt-6 gap-4 my-8">
+          <Buttons categoryName="Mountain" setCategory={setSearch1} />
+          <Buttons categoryName="Flowers" setCategory={setSearch1} />
+          <Buttons categoryName="Sea" setCategory={setSearch1} />
+          <Buttons categoryName="Faces" setCategory={setSearch1} />
+          <Buttons categoryName="Cats" setCategory={setSearch1} />
+          <Buttons categoryName="Dogs" setCategory={setSearch1} />
         </section>
         <section>
           <Galery search={search1} />
         </section>
       </main>
 
-      <footer className="flex justify-center items-center mt-16 mb-4">
-        <p>
+      <footer className="flex justify-center items-center mt-16 bg-pink-300">
+        <p className="my-12">
           created by <strong>yamirengifo</strong>
         </p>
       </footer>
--- src/components.jsx/Buttons.jsx
+++ src/components.jsx/Buttons.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Buttons(props) {
+export default function Buttons({ categoryName, setCategory }) {
   return (
     <div>
-      <button className="bg-pink-400 rounded-2xl py-1 px-2.5 font-bold text-gray-700 w-[100px]">
-        {props.category}
+      <button
+        className="bg-pink-400 rounded-2xl py-1 px-2.5 font-bold text-gray-700 w-[100px]"
+        onClick={(e) => setCategory(categoryName)}
+      >
+        <p>{categoryName}</p>
       </button>
     </div>
   );
